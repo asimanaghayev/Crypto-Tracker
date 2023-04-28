b39435c27e23ee353f2d0a6f1b0e2b89d907aafb
asimanaghayev <asimanagayev11@gmail.com>
2023-04-29 01:10:13 +0400

    Fixed worker issues

--- app/src/main/java/com/asiman/cryptotracker/base/App.kt
+++ app/src/main/java/com/asiman/cryptotracker/base/App.kt
@@ -1,18 +1,28 @@
 package com.asiman.cryptotracker.base
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
 @HiltAndroidApp
-class App : Application() {
+class App : Application(), Configuration.Provider {
 
     @Inject
     lateinit var exceptionHandler: GlobalExceptionHandler
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
 
     override fun onCreate() {
         super.onCreate()
 
         Thread.setDefaultUncaughtExceptionHandler(exceptionHandler)
     }
+
+    override fun getWorkManagerConfiguration() =
+        Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
 }
